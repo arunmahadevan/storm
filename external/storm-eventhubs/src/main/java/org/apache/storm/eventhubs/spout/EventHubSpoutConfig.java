81bc894a8c427d5bcb8fa4e34e8b1e2d13a78aad
Arun Mahadevan <arunm@apache.org>
2017-02-15 23:50:49 +0530

    [STORM-2365] Support for specifying output stream in event hubs spout

--- external/storm-eventhubs/src/main/java/org/apache/storm/eventhubs/spout/EventHubSpoutConfig.java
+++ external/storm-eventhubs/src/main/java/org/apache/storm/eventhubs/spout/EventHubSpoutConfig.java
@@ -45,6 +45,8 @@ public class EventHubSpoutConfig implements Serializable {
   private String consumerGroupName = null; // if null then use default
                         // consumer group
 
+  private String outputStreamId;
+
   // These are mandatory parameters
   public EventHubSpoutConfig(String username, String password,
       String namespace, String entityPath, int partitionCount) {
@@ -238,5 +240,13 @@ public class EventHubSpoutConfig implements Serializable {
   public EventHubSpoutConfig withTargetAddress(String targetFqnAddress) {
     setTargetAddress(targetFqnAddress);
     return this;
+  }
+
+  public String getOutputStreamId() {
+    return outputStreamId;
+  }
+
+  public void setOutputStreamId(String outputStreamId) {
+    this.outputStreamId = outputStreamId;
   }
 }
